712cb281b4d8ae4403551c2b5d568ee1d9c4df51
Michael Mercier <michael.mercier@ryax.tech>
2022-12-16 15:36:57 +0100

    Use job name instead of env var for NOOP

--- connectors/slurm.go
+++ connectors/slurm.go
@@ -7,24 +7,22 @@ import (
 )
 
 type SlurmConfig struct {
-	nbCpuPerJob int	
+	nbCpuPerJob          int
 	jobDurationInSeconds int
-	
 }
 
 func Punch() error {
 	// TODO put this in a config file (or env var)
 	config := SlurmConfig{nbCpuPerJob: 1, jobDurationInSeconds: 900}
 
-	cmd := fmt.Sprintf("BEBIDA_NOOP=1 srun -n %d sleep %d", config.nbCpuPerJob, config.jobDurationInSeconds)
+	cmd := fmt.Sprintf("srun --job-name BEBIDA_NOOP -n %d sleep %d", config.nbCpuPerJob, config.jobDurationInSeconds)
 	out, err := ExecuteCommand(cmd)
 
-    if err != nil {
-        log.Errorf("Unable to submit job: %s", err)
+	if err != nil {
+		log.Errorf("Unable to submit job: %s", err)
 		return err
-    }
+	}
 
-    log.Infof("Punch command output: %s", string(out))
+	log.Infof("Punch command output: %s", string(out))
 	return nil
 }
-
