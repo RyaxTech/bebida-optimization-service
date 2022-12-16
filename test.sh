eb070b0c3e9daef830bc11d6a6f9023aa34339ec
Michael Mercier <michael.mercier@ryax.tech>
2022-12-16 16:26:55 +0100

    fix tests waiting for PKEY

--- test.sh
+++ test.sh
@@ -14,14 +14,14 @@ export K3S_TOKEN=${RANDOM}${RANDOM}${RANDOM}
 # Start services
 docker-compose up -d
 
+echo Waiting for Kubernetes API...
+until [[ $(kubectl get endpoints/kubernetes -o=jsonpath='{.subsets[*].addresses[*].ip}' ) ]]; do sleep 2; echo -n "--- " ; done
+echo Kubernetes Connection is UP!
+
 # Export variables for ssh access
 export BEBIDA_SSH_PKEY=$(docker-compose exec slurmctld cat /root/.ssh/id_rsa | base64)
 export BEBIDA_SSH_USER="root"
 export BEBIDA_SSH_HOSTNAME="127.0.0.1"
 export BEBIDA_SSH_PORT="2222"
-
-echo Waiting for Kubernetes API...
-until [[ $(kubectl get endpoints/kubernetes -o=jsonpath='{.subsets[*].addresses[*].ip}' ) ]]; do sleep 2; echo -n "--- " ; done
-echo Kubernetes Connection is UP!
 
 go test .
