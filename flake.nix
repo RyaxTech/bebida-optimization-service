bea860caeb1f861490b5c5a18d4d55dae784b9a0
Michael Mercier <michael.mercier@ryax.tech>
2023-11-21 14:30:47 +0100

    Update docs and deps

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Bebida Shaker";
 
   # Nixpkgs / NixOS version to use.
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
   outputs = { self, nixpkgs }:
     let
